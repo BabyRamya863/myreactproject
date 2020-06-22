0ff74ba04527767d1289ccbad5cb54ef865de9d2
BabyRamya863 <manuramya888@gmail.com>
2020-06-22 18:44:09 +0530

    Updates

---
+++ precache-manifest.d8f6d25f2b1aa970bd61f768059cb85c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c658b41b7080003dff6ff1132c74a6",
+    "url": "/myreactproject/index.html"
+  },
+  {
+    "revision": "24e66a7c5fc0effb8a69",
+    "url": "/myreactproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6f7af61d354142a089",
+    "url": "/myreactproject/static/js/2.ab7caccb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactproject/static/js/2.ab7caccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e66a7c5fc0effb8a69",
+    "url": "/myreactproject/static/js/main.d5f2f11b.chunk.js"
+  },
+  {
+    "revision": "4ee64b5c2989d91a8c95",
+    "url": "/myreactproject/static/js/runtime-main.a1a95b0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactproject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f88fc6b60d4b7515f1b0d14efd320a0",
+    "url": "/myreactproject/static/media/profileicon.6f88fc6b.svg"
+  }
+]);
